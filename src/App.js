3d86c0d5b3ff144ead9549582a832327aa5b7257
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-05-25 20:59:31 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-black-600">
+      App
+    </div>
+  );
+};
+
+export default App;
