28281efee1144800486beedca0ce6f9eb79ceb22
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-06-11 21:23:02 +0300

    update screen and add bg

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Header, CreateContainer, MainContainer } from 'components';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import HeroBg from './img/44.jpg'
+import HeroBg from './img/44.jpg';
+import HeroSecondBg from './img/bg-4.jpg';
 
 const App = () => {
-  //initial={false}
-  //md:mt-23 md:p-5
+  const location = useLocation();
+
+  let backgroundImage = HeroBg;
+  if (location.pathname === '/createItem') {
+    backgroundImage = HeroSecondBg;
+  }
+
   return (
     <AnimatePresence>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
-        <main className="mt-17 p-6 px-10 md:mt-20 md:px-16 lg:px-20 py-4 w-full h-full  bg-center bg-mainGrey justify-center items-center" style={{backgroundImage: `url(${HeroBg}`,backgroundRepeat:"no-repeat", backgroundSize: "cover"}}>
+        <main
+          className="mt-17 p-6 px-10 md:mt-20 md:px-16 lg:px-20 py-4 w-full h-full bg-center bg-mainGrey justify-center items-center"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+          }}
+        >
           <Routes>
             <Route path="/*" element={<MainContainer />} />
             <Route path="/createItem" element={<CreateContainer />} />
@@ -25,3 +38,5 @@ const App = () => {
 export default App;
 
 //main className="mt-17 p-6 px-10 md:mt-20 md:px-16 lg:px-20 py-4 w-full h-600 md:h-800 bg-center bg-mainGrey justify-center items-center"
+
+
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -7,7 +7,7 @@ import { heroData } from '../utils/data'
 
 const HomeContainer = () => {
   return (
-    <section className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-20 w-full' id='home' >
+    <section className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-20 w-full min-h-screen h-auto' id='home' >
       <div className='py-2 md:py-12 flex-1 flex flex-col items-start lg:justify-center gap-6'>
         <div className='flex items-center gap-2 justify-center bg-logoColor px-4 py-1 rounded-full'>
         <p className='text-white text-center text-base font-semibold'>Bike Delivery</p>
@@ -26,14 +26,14 @@ const HomeContainer = () => {
       </div>
       <div className='py-2 md:py-12 flex-1 flex items-center relative'>
           {/* <img src={PosterBg} alt="hero-Bg" className='rounded-2xl'/> */}
-          <div className='ml-auto h-420  md:h-650 bg-white w-full md:w-510 rounded-2xl ' style={{
+          <div className='ml-auto h-420  md:h-600 bg-white w-full md:w-510 rounded-2xl ' style={{
           backdropFilter: 'blur(10px)',
           backgroundColor: 'rgba(90, 72, 72, 0.3)',
           boxShadow: 'rgba(0, 0, 0, 0.5) 0px 5px 15px'
         }}>
 
         </div>
-        <div className='h-370 lg:h-650 absolute flex top-4  md:top-[100px] lg:top-16 left-0 items-center justify-center py-2 gap-4 flex-wrap lg:px-0 xlL:px-9 2xl:px-44' >
+        <div className='h-370 lg:h-600 absolute flex top-4  md:top-[100px] lg:top-16 left-0 items-center justify-center py-2 gap-4 flex-wrap lg:px-0 xlL:px-9 2xl:px-44' >
           {heroData && heroData.map(n =>(
             <div key={n.id} className=' lg:w-190 w-170 p-1 md:p-4 bg-itemBg rounded-3xl flex flex-col items-center justify-center mt-8 md:mt-10 lg:mt-0' style={{
             backdropFilter: 'blur(10px)',
