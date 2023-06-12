621880d486863cf055971e9b3b569175ee29a91c
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-06-12 11:40:41 +0300

    update

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -34,7 +34,7 @@ const HomeContainer = () => {
         }}>
 
         </div>
-        <div className=' h-370 lg:h-600 absolute flex top-4  md:top-[100px]  xlM:top-[150px]
+        <div className=' h-370 lg:h-600 absolute flex top-4  md:top-[150px]  xlM:top-[150px]
         3xl:top-[200px]  left-0 items-center justify-center py-2 gap-4 flex-wrap  xlL:px-16 2xl:px-44' >
           {heroData && heroData.map(n =>(
             <div key={n.id} className=' xl:w-190 w-170 p-1 md:p-4 bg-itemBg rounded-3xl flex flex-col items-center justify-center mt-8 md:mt-10 lg:mt-0' style={{
