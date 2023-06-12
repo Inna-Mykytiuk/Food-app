805dae5911063aa7dfa83516749fd24d262ed0c3
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-06-12 11:35:11 +0300

    update

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -8,7 +8,7 @@ import { heroData } from '../utils/data'
 const HomeContainer = () => {
   return (
     <section className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-20 w-full min-h-screen h-auto' id='home' >
-      <div className='py-2 flex-1 flex flex-col items-start lg:justify-center gap-6'>
+      <div className='py-2 flex-1 flex flex-col items-start justify-center gap-6'>
         <div className='flex items-center gap-2 justify-center bg-logoColor px-4 py-1 rounded-full'>
         <p className='text-white text-center text-base font-semibold'>Bike Delivery</p>
         <div className='w-8 h-8 bg-logoColor rounded-full overflow-hidden drop-shadow-md'>
@@ -26,7 +26,7 @@ const HomeContainer = () => {
       </div>
       <div className='py-2 md:py-12 flex-1 flex items-center lg:justify-center relative'>
           {/* <img src={PosterBg} alt="hero-Bg" className='rounded-2xl'/> */}
-          <div className='ml-auto h-420  md:h-600 bg-white w-full
+          <div className='ml-auto h-420  lg:h-600 bg-white w-full
           md:w-510 rounded-2xl ' style={{
           backdropFilter: 'blur(10px)',
           backgroundColor: 'rgba(90, 72, 72, 0.3)',
