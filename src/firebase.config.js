059fa2f841848b4516081936d8c9bcd65f953710
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-06-16 21:27:17 +0300

    update

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,16 +2,6 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
   apiKey: 'AIzaSyDRKdluX2EoWCQ25nan760dYayP9aMAn_s',
   authDomain: 'foodapp-b4f5f.firebaseapp.com',
