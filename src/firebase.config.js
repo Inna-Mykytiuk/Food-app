cc96c2c417e2c4dc87c79dd62097ddef89310c48
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-06-16 22:16:31 +0300

    hide secrets

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,17 +2,6 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDRKdluX2EoWCQ25nan760dYayP9aMAn_s',
-//   authDomain: 'foodapp-b4f5f.firebaseapp.com',
-//   databaseURL:
-//     'https://foodapp-b4f5f-default-rtdb.europe-west1.firebasedatabase.app',
-//   projectId: 'foodapp-b4f5f',
-//   storageBucket: 'foodapp-b4f5f.appspot.com',
-//   messagingSenderId: '1095396547734',
-//   appId: '1:1095396547734:web:588df6a61ed5a71f7076b7',
-// }
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
